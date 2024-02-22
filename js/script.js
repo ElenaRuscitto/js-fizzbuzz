9ada56a3e2f59ee96888731583635cd768566f34
ElenaRuscitto <ruscitto.elena@gmail.com>
2024-02-22 15:34:08 +0100

    [add] multipli di 3 e 5

--- js/script.js
+++ js/script.js
@@ -1,18 +1,25 @@
 const containerBox =document.getElementById ('container');
 
 for(let i = 1; i <= 100; i++){
-  containerBox.innerHTML += `
-  <div class="box">${i}</div>
+  
+  if ((i % 3  === 0) && (i % 5  === 0)){
+    containerBox.innerHTML += `
+    <div class="box box-6">${i}</div>
+
    `
-  if (i % 3  === 0){
+  } else if (i % 3  === 0){
     containerBox.innerHTML += `
     <div class="box box-3">${i}</div>
 
    `
-  } else if(i % 5  === 0){
+  } else if (i % 5  === 0){
     containerBox.innerHTML += `
     <div class="box box-5">${i}</div>
 
+   `
+  } else {
+    containerBox.innerHTML += `
+  <div class="box">${i}</div>
    `
   }
 }
