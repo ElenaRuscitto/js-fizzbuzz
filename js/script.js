9df0335b61579047d9b1e4b01709c4da8d912005
ElenaRuscitto <ruscitto.elena@gmail.com>
2024-02-22 16:08:31 +0100

    [add] scritte

--- js/script.js
+++ js/script.js
@@ -4,17 +4,18 @@ for(let i = 1; i <= 100; i++){
   
   if ((i % 3  === 0) && (i % 5  === 0)){
     containerBox.innerHTML += `
-    <div class="box box-6">${i}</div>
-
+    <div class="box box-6">FizzBuzz</div>
+    
    `
+   
   } else if (i % 3  === 0){
     containerBox.innerHTML += `
-    <div class="box box-3">${i}</div>
+    <div class="box box-3">Fizz</div>
 
    `
   } else if (i % 5  === 0){
     containerBox.innerHTML += `
-    <div class="box box-5">${i}</div>
+    <div class="box box-5">Buzz</div>
 
    `
   } else {
